dd545b8f76e630d205ef7fb2e504f2825a8bd2c5
Peter Lieverdink <me@cafuego.net>
2015-11-18 09:49:09 +1100

    [#2617100] Check for overridden admin_user view (and any overridden views).

---
+++ plugins/checker/AdminViewsChecker.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains AdminViewsChecker
+ */
+
+/**
+ * Checks for a dangerous option in the admin_views user view.
+ */
+class AdminViewsChecker implements CheckerInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChecks() {
+    $checks = array();
+    // Check whether the admin_views user view still contains the delete action.
+    if (module_exists('admin_views')) {
+      $view = views_get_view('admin_views_user');
+      $vbo_operations = $view->display['default']->display_options['fields']['views_bulk_operations']['vbo_operations'];
+
+      if (isset($vbo_operations['action::views_bulk_operations_delete'])) {
+        $checks[] = array(
+          'name'        => t('Administration: Users'),
+          'description' => t('This view exposes the dangerous user delete action.'),
+          'type'        => 'views',
+          'alert_level' => 'error',
+        );
+      }
+    }
+    return $checks;
+  }
+}
