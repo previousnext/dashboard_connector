547a89840b56bdf559dca234fe2c95fa8bd755b3
Peter Lieverdink <me@cafuego.net>
2015-11-18 09:49:09 +1100

    [#2617100] Check for overridden admin_user view (and any overridden views).

---
+++ plugins/checker/ViewsChecker.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains ViewsChecker
+ */
+
+/**
+ * Checks whether any views are in overridden state.
+ */
+class ViewsChecker implements CheckerInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChecks() {
+    $checks = array();
+    // Check whether any views are overridden.
+    if (module_exists('views')) {
+      $views = views_get_all_views();
+      foreach ($views as $view) {
+        if ($view->type == t('Overridden')) {
+          $checks[] = array(
+            'name'        => !empty($view->human_name) ? check_plain($view->human_name) : $view->name,
+            'description' => t('View @name is overridden.', array('@name' => $view->name)),
+            'type'        => 'views',
+            'alert_level' => 'warning',
+          );
+        }
+      }
+    }
+    return $checks;
+  }
+}
