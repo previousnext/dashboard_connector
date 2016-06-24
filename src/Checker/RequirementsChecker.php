5889bae8befa8867fe8df52e46f7fa8dc02bb301
Kim Pepper <kim@pepper.id.au>
2016-06-24 11:58:32 +1000

    Fix render error on requirements

--- src/Checker/RequirementsChecker.php
+++ src/Checker/RequirementsChecker.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\dashboard_connector\Checker;
 
+use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\system\SystemManager;
 
@@ -18,16 +19,26 @@ class RequirementsChecker extends CheckerBase {
   protected $systemManager;
 
   /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * ModuleStateChecker constructor.
    *
    * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
    *   The translations service.
    * @param \Drupal\system\SystemManager $system_manager
    *   The system manager.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
    */
-  public function __construct(TranslationInterface $string_translation, SystemManager $system_manager) {
+  public function __construct(TranslationInterface $string_translation, SystemManager $system_manager, RendererInterface $renderer) {
     parent::__construct($string_translation);
     $this->systemManager = $system_manager;
+    $this->renderer = $renderer;
   }
 
   /**
@@ -38,11 +49,27 @@ class RequirementsChecker extends CheckerBase {
     // Check run-time requirements and status information.
     $requirements = $this->systemManager->listRequirements();
     foreach ($requirements as $requirement) {
-      if (isset($requirement['severity']) && $requirement['severity'] === REQUIREMENT_ERROR) {
-        $checks[] = $this->buildCheck('requirement', $requirement['title'], strip_tags($requirement['value']), 'error');
+      if (isset($requirement['severity']) && in_array($requirement['severity'], [REQUIREMENT_ERROR, REQUIREMENT_WARNING], TRUE)) {
+        $alert_level = $requirement['severity'] === REQUIREMENT_ERROR ? 'error' : 'warning';
+        $checks[] = $this->buildCheck('requirement', $this->render($requirement['title']), $this->render($requirement['value']), $alert_level);
       }
     }
     return $checks;
+  }
+
+  /**
+   * Renders an element.
+   *
+   * @param string|array $element
+   *
+   * @return string
+   *   A rendered plain text string.
+   */
+  protected function render($element) {
+    if (is_array($element)) {
+      return strip_tags((string) $this->renderer->renderPlain($element));
+    }
+    return strip_tags($element);
   }
 
 }
