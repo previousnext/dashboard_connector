4f7d546b7f03bdf779ee318de2bdc92f4b9e496f
Kim Pepper <kim@pepper.id.au>
2016-06-28 13:04:53 +1000

    Fix incorrect performance config keys (#44)
    * Fix incorrect performance config keys
    * Fix tests

--- tests/src/Unit/PerformanceCheckerTest.php
+++ tests/src/Unit/PerformanceCheckerTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\dashboard_connector\Unit;
 
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\dashboard_connector\Checker\PerformanceChecker;
 use Drupal\Tests\UnitTestCase;
 use Prophecy\Argument;
@@ -26,9 +28,9 @@ class PerformanceCheckerTest extends UnitTestCase {
   public function setUp() {
     parent::setUp();
 
-    $config = $this->prophesize('Drupal\Core\Config\ImmutableConfig');
+    $config = $this->prophesize(ImmutableConfig::class);
     $config->get(Argument::any())->willReturn([]);
-    $translation = $this->prophesize('Drupal\Core\StringTranslation\TranslationInterface');
+    $translation = $this->prophesize(TranslationInterface::class);
     $config_factory = $this->getConfigFactoryStub(['system.performance' => []]);
     $this->checker = new PerformanceChecker($translation->reveal(), $config_factory);
   }
@@ -41,17 +43,12 @@ class PerformanceCheckerTest extends UnitTestCase {
 
     $this->assertNotEmpty($checks);
 
-    $blocks_check = $checks[0];
-    $this->assertEquals($blocks_check['name'], 'block_cache');
-    $this->assertEquals($blocks_check['type'], 'performance');
-    $this->assertEquals($blocks_check['alert_level'], 'warning');
-
-    $preprocess_css_check = $checks[1];
+    $preprocess_css_check = $checks[0];
     $this->assertEquals($preprocess_css_check['name'], 'preprocess_css');
     $this->assertEquals($preprocess_css_check['type'], 'performance');
     $this->assertEquals($preprocess_css_check['alert_level'], 'warning');
 
-    $preprocess_js_check = $checks[2];
+    $preprocess_js_check = $checks[1];
     $this->assertEquals($preprocess_js_check['name'], 'preprocess_js');
     $this->assertEquals($preprocess_js_check['type'], 'performance');
     $this->assertEquals($preprocess_js_check['alert_level'], 'warning');
